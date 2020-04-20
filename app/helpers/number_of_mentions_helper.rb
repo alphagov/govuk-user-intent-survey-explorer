2b58067c33fdf897617bf33e59873a938b2ab040
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-04-20 14:59:16 +0100

    Use actual data for Phrase mentions
    This commit uses the actual database data for showing the table and chart of phrase mentions.

--- app/helpers/number_of_mentions_helper.rb
+++ app/helpers/number_of_mentions_helper.rb
@@ -1,14 +1,23 @@
+require 'time'
+require 'active_support'
+require 'active_support/core_ext/integer/inflections'
+
 module NumberOfMentionsHelper
   def map_mentions_data_to_table(data)
     data.map do |row|
+      date = Time.parse(row.date)
+
       [
-        { text: row[:date] },
-        { text: row[:mentions], format: 'numeric' }
+        { text: date.strftime("#{date.day.ordinalize} %b %Y") },
+        { text: row.total_mentions, format: 'numeric' }
       ]
     end
   end
 
   def map_mentions_data_to_chart(data)
-    data.each_with_object({}){|d, hash| hash[d[:date]] = d[:mentions] }
+    data.each_with_object({}) do |m, hash|
+      date = DateTime.parse(m.date).strftime('%-d %b')
+      hash[date] = m.total_mentions
+    end
   end
 end
