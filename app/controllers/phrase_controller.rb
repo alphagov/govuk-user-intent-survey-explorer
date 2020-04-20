328affd47cb858b2dc066d0aa5b887349db911ed
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-04-20 16:58:59 +0100

    Update the phrase and pages visited pages with page data
    This commit updates the phrase and pages visited pages to show pages related to users' journeys, where those user submitted survey answers containing phrases which are being shown in these two pages.
    
    The query isn't truly accurate as we can only join as far up as the survey - everything on the other side (visits, page visits and pages) is joined to the survey table by visitor_id, so there may be pages included as a result of this query which the user didn't go to as part of their phrased-survey journey.

--- app/controllers/phrase_controller.rb
+++ app/controllers/phrase_controller.rb
@@ -1,26 +1,24 @@
 class PhraseController < ApplicationController
   def show
     @phrase = Phrase.find(params[:id])
-    @pages_visited = Page.limit(10)
     @devices_used = [
       { device_type: 'Desktop', percentage_use: 0.35 },
       { device_type: 'Mobile', percentage_use: 0.6 },
       { device_type: 'Tablet', percentage_use: 0.04 }
     ]
-    @mentions = [
-      'My passport has been mislaid by the Pakistan High Commission. I sent it there to get a visa to visit Pakistan. Now they are asking for my passport number How can I find out my passport number, as I have asthma, if I do not have the passport in my possession?',
-      'My passport has been mislaid by the Pakistan High Commission. I sent it there to get a visa to visit Pakistan. Now they are asking for my passport number How can I find out my passport number, as I have asthma, if I do not have the passport in my possession?',
-      'My passport has been mislaid by the Pakistan High Commission. I sent it there to get a visa to visit Pakistan. Now they are asking for my passport number How can I find out my passport number, as I have asthma, if I do not have the passport in my possession?'
-    ]
-    @total_mentions = 127530
-    @mentions_by_day = {
-      '03/04' => '27632',
-      '04/04' => '25055',
-      '05/04' => '25602',
-      '06/04' => '26051',
-      '07/04' => '25437',
-      '08/04' => '13006',
-      '09/04' => '9991'
-    }
+
+    @pages_visited = pages_visited
+    @mentions = mentions
+    @total_mentions = @mentions.inject(0){|sum, mention| sum + mention.total_mentions}
+  end
+
+private
+
+  def pages_visited
+    Page.find_by_sql('select pages.id, pages.base_path, count(pv.visit_id) as total_pageviews from survey_phrases m join phrases on phrases.id = m.phrase_id join survey_answers sa on sa.id = m.survey_answer_id join surveys s on s.id = sa.survey_id join visits v on v.visitor_id = s.visitor_id join page_visits pv on pv.visit_id = v.id join pages on pages.id = pv.page_id where phrases.id = 5 group by (pages.id) order by total_pageviews desc;')
+  end
+
+  def mentions
+    SurveyAnswer.find_by_sql(['select date(s.started_at) as date, count(m.id) as total_mentions from survey_phrases m join phrases p on p.id = m.phrase_id join survey_answers sa on sa.id = m.survey_answer_id join surveys s on s.id = sa.survey_id where p.id = ? group by(date(s.started_at)) limit 10;', @phrase.id])
   end
 end
