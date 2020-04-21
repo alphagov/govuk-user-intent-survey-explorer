ad94d67d0c7811a9066e80a62e015ccdbd600ea2
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-04-21 12:47:47 +0100

    Fix references to survey dates to use postgres result types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def format_datetime(datetime)
     datetime.strftime("%d-%m-%y at %H:%M")
   end
+
+  def map_mentions_data_to_chart(mentions_data)
+    mentions_data.each_with_object({}) do |m, hash|
+      date = m.date.strftime('%-d %b')
+      hash[date] = m.total_mentions
+    end
+  end
 end
--- app/helpers/number_of_mentions_helper.rb
+++ app/helpers/number_of_mentions_helper.rb
@@ -1,23 +1,13 @@
-require 'time'
 require 'active_support'
 require 'active_support/core_ext/integer/inflections'
 
 module NumberOfMentionsHelper
   def map_mentions_data_to_table(data)
     data.map do |row|
-      date = Time.parse(row.date)
-
       [
-        { text: date.strftime("#{date.day.ordinalize} %b %Y") },
+        { text: row.date.strftime("#{row.date.day.ordinalize} %b %Y") },
         { text: row.total_mentions, format: 'numeric' }
       ]
-    end
-  end
-
-  def map_mentions_data_to_chart(data)
-    data.each_with_object({}) do |m, hash|
-      date = DateTime.parse(m.date).strftime('%-d %b')
-      hash[date] = m.total_mentions
     end
   end
 end
