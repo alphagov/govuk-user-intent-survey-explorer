c0f39176a6acc72be10eb3f1355ba391e21a0690
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-04-21 10:02:11 +0100

    Move map_mentions_data_to_chart into phrase helper for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,4 @@ module ApplicationHelper
   def format_datetime(datetime)
     datetime.strftime("%d-%m-%y at %H:%M")
   end
-
-  def map_mentions_data_to_chart(mentions_data)
-    mentions_data.each_with_object({}) do |m, hash|
-      date = DateTime.parse(m.date).strftime('%-d %b')
-      hash[date] = m.total_mentions
-    end
-  end
 end
--- app/helpers/phrase_helper.rb
+++ app/helpers/phrase_helper.rb
@@ -7,4 +7,11 @@ module PhraseHelper
       ]
     end
   end
+
+  def map_mentions_data_to_chart(mentions_data)
+    mentions_data.each_with_object({}) do |m, hash|
+      date = DateTime.parse(m.date).strftime('%-d %b')
+      hash[date] = m.total_mentions
+    end
+  end
 end
