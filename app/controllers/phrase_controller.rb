fe41e41fd90092a87fae5befd86547ec7adb563e
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-04-23 19:47:22 +0100

    Reduce number of pages and mentions on phrase detail page
    This commit reduces the number of pages and mentions on the phrase detail page. By doing this we don't overload the user with info, keeping the page as a summary page and also keeping to the design.

--- app/controllers/phrase_controller.rb
+++ app/controllers/phrase_controller.rb
@@ -4,7 +4,7 @@ class PhraseController < ApplicationController
     @devices = devices
     @pages_visited = pages_visited
     @mentions = mentions
-    @survey_answers_containing_phrase = SurveyAnswer.find_by_sql(["select * from survey_answers sa join questions q on q.id = sa.question_id join survey_phrases sp on sp.survey_answer_id = sa.id join phrases p on p.id = sp.phrase_id where p.id = ? and q.question_number = 3 and sa.answer not like '-' limit 10", "#{@phrase.id}"])
+    @survey_answers_containing_phrase = SurveyAnswer.find_by_sql(["select * from survey_answers sa join questions q on q.id = sa.question_id join survey_phrases sp on sp.survey_answer_id = sa.id join phrases p on p.id = sp.phrase_id where p.id = ? and q.question_number = 3 and sa.answer not like '-' limit 3", "#{@phrase.id}"])
   end
 
 private
@@ -20,7 +20,7 @@ private
   end
 
   def pages_visited
-    Page.find_by_sql("select pages.id, pages.base_path, count(pv.visit_id) as total_pageviews from survey_phrases m join phrases on phrases.id = m.phrase_id join survey_answers sa on sa.id = m.survey_answer_id join surveys s on s.id = sa.survey_id join visits v on v.visitor_id = s.visitor_id join page_visits pv on pv.visit_id = v.id join pages on pages.id = pv.page_id where phrases.id = #{@phrase.id} group by (pages.id) order by total_pageviews desc;")
+    Page.find_by_sql("select pages.id, pages.base_path, count(pv.visit_id) as total_pageviews from survey_phrases m join phrases on phrases.id = m.phrase_id join survey_answers sa on sa.id = m.survey_answer_id join surveys s on s.id = sa.survey_id join visits v on v.visitor_id = s.visitor_id join page_visits pv on pv.visit_id = v.id join pages on pages.id = pv.page_id where phrases.id = #{@phrase.id} group by (pages.id) order by total_pageviews desc limit 10;")
   end
 
   def mentions
