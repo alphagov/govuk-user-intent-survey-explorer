4ce83056e6fb59637a1d9c26bd026602f8466729
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-04-27 17:04:53 +0100

    Set-up testing environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,23 @@
 ENV["RAILS_ENV"] ||= "test"
 
-require File.expand_path("../config/environment", __dir__)
+require File.expand_path("../../config/environment", __FILE__)
+require "rspec/rails"
 
 require "byebug"
 require "simplecov"
 require "webmock/rspec"
+require 'capybara/rails'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 SimpleCov.start
-WebMock.disable_net_connect!(allow_localhost: true)
+
 ActiveRecord::Migration.maintain_test_schema!
-Rails.application.load_tasks
-Rails.application.load_seed
+
 
 RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-  #
-  # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
-  # have no way to turn it off -- the option exists only for backwards
-  # compatibility in RSpec 3). It causes shared context metadata to be
-  # inherited by the metadata hash of host groups and examples, rather than
-  # triggering implicit auto-inclusion in groups with matching metadata.
-  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.expose_dsl_globally = false
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = true
 
-  config.order = :random
+  config.include FactoryBot::Syntax::Methods
 end
