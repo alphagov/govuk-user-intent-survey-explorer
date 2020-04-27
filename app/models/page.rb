01b2dd4c8a93feeeb8aac46840b3cc8e47fedd77
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-04-27 17:06:06 +0100

    Comment out dependencies on ES - come back to this later

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,14 +1,14 @@
-require 'elasticsearch/model'
+# require 'elasticsearch/model'
 
 class Page < ApplicationRecord
   has_many :page_visits
   has_many :visits, through: :page_visits
 
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  # include Elasticsearch::Model
+  # include Elasticsearch::Model::Callbacks
 
-  mapping do
-    indexes :base_path, type: 'text', analyzer: 'english' do
-    end
-  end
+  # mapping do
+  #   indexes :base_path, type: 'text', analyzer: 'english' do
+  #   end
+  # end
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,4 @@
-require 'elasticsearch/model'
+# require 'elasticsearch/model'
 
 class Survey < ApplicationRecord
   belongs_to :organisation
@@ -8,20 +8,20 @@ class Survey < ApplicationRecord
   has_many :survey_answers, -> { order(:question_id) }
   has_many :survey_user_groups, dependent: :destroy
 
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  # include Elasticsearch::Model
+  # include Elasticsearch::Model::Callbacks
 
-  mapping do
-    indexes :responses, type: 'text', analyzer: 'english'
-    indexes :started_at, type: 'date'
-  end
-
-  def as_indexed_json(options = {})
-    {
-      responses: survey_answers.preload(:question).to_a.
-        keep_if { |answer| answer.question.free_text_response }.
-        map(&:answer).join(" "),
-      started_at: started_at.strftime('%F'),
-    }.as_json
-  end
+  # mapping do
+  #   indexes :responses, type: 'text', analyzer: 'english'
+  #   indexes :started_at, type: 'date'
+  # end
+  #
+  # def as_indexed_json(options = {})
+  #   {
+  #     responses: survey_answers.preload(:question).to_a.
+  #       keep_if { |answer| answer.question.free_text_response }.
+  #       map(&:answer).join(" "),
+  #     started_at: started_at.strftime('%F'),
+  #   }.as_json
+  # end
 end
