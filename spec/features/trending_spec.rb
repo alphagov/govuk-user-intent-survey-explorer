2bb82b1541496a26757de30e527851ea1246707b
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-04-27 17:06:48 +0100

    WIP: initial test for trending page. Issues with FactoryBot 5 not creating associations automatically

---
+++ spec/features/trending_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+RSpec.feature 'trend page' do
+  # before(:each) do
+  #   p = FactoryBot.create(:page, base_path: '/department-of-wizards')
+  #   v = FactoryBot.create(:visit)
+  #   FactoryBot.create(:page_visit, page: p, visit: v)
+  # end
+
+  # let(:top_page) { FactoryBot.create(:page, base_path: '/department-of-wizards') }
+  # let(:top_page_visit) { FactoryBot.create(:visit) }
+  # let(:page_visit) { FactoryBot.create(:page_visit, page: top_page) }
+
+  before(:each) do
+    @top_page = FactoryBot.create(:page, base_path: '/department-of-wizards')
+    @page_visit = FactoryBot.create(:page_visit, page: @top_page)
+
+    @phrase = FactoryBot.create(:phrase, phrase_text: 'abra cadabra')
+    @survey_answer = FactoryBot.create(:survey_answer, answer: 'I wanted to say abra cadabra and find what I was looking for')
+    @survey_phrase = FactoryBot.create(:survey_phrase, phrase: @phrase, survey_answer: @survey_answer)
+    @user_group = FactoryBot.create(:user_group, group: 'Over 40')
+    @survey = FactoryBot.create(:survey, started_at: '2020-04-02 00:00:00')
+    @survey_user_group = FactoryBot.create(:survey_user_group, survey: @survey, user_group: @user_group)
+  end
+
+  scenario 'displays overview of trending data' do
+    # top_page = FactoryBot.create(:page, base_path: '/department-of-wizards')
+    # page_visit = FactoryBot.create(:page_visit, page: top_page)
+
+
+    visit '/'
+    expect(page).to have_content('User intent survey data')
+    expect(page).to have_link(@top_page.base_path, href: "https://www.gov.uk#{@top_page.base_path}")
+    expect(page).to have_link(@phrase.phrase_text)
+    expect(page).to have_content(@user_group.group)
+  end
+end
