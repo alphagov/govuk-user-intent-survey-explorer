14213a5d9d950cea0a73cc1eedd2c9a942c2c0a9
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-04-28 14:11:02 +0100

    Fix pagination on phrase usage page

--- app/controllers/phrase_usage_controller.rb
+++ app/controllers/phrase_usage_controller.rb
@@ -7,7 +7,7 @@ class PhraseUsageController < ApplicationController
 private
 
   def survey_answers_containing_phrase
-    SurveyAnswer.find_by_sql(["select * from survey_answers sa join questions q on q.id = sa.question_id join survey_phrases sp on sp.survey_answer_id = sa.id join phrases p on p.id = sp.phrase_id where p.id = ? and q.question_number = 3 and sa.answer not like '-' limit 10", "#{params[:id]}"])
+    SurveyAnswer.find_by_sql(["select * from survey_answers sa join questions q on q.id = sa.question_id join survey_phrases sp on sp.survey_answer_id = sa.id join phrases p on p.id = sp.phrase_id where p.id = ? and q.question_number = 3 and sa.answer not like '-'", "#{params[:id]}"])
   end
 
   def search_params
