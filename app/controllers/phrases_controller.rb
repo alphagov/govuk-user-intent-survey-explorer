1ccb1a2ad14b1a12e1cdcc76312b81daa04a3e90
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-04-29 12:58:04 +0100

    Move phrase usages within the phrase controller

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,10 +1,15 @@
-class PhraseController < ApplicationController
+class PhrasesController < ApplicationController
   def show
     @phrase = Phrase.find(params[:id])
     @devices = devices
     @pages_visited = pages_visited
     @mentions = mentions
     @survey_answers_containing_phrase = SurveyAnswer.find_by_sql(["select * from survey_answers sa join questions q on q.id = sa.question_id join survey_phrases sp on sp.survey_answer_id = sa.id join phrases p on p.id = sp.phrase_id where p.id = ? and q.question_number = 3 and sa.answer not like '-' limit 3", "#{@phrase.id}"])
+  end
+
+  def usage
+    @phrase = Phrase.find(params[:id])
+    @presenter = PhraseUsagePresenter.new(survey_answers_containing_phrase, search_params)
   end
 
 private
@@ -28,5 +33,19 @@ private
       .mentions_by_date_range_for_phrase(@phrase, Date.new(2020, 4, 1), Date.new(2020, 4, 7))
       .sort_by{ |date, _| date }
       .to_h
+  end
+
+  def survey_answers_containing_phrase
+    SurveyAnswer.find_by_sql(["select * from survey_answers sa join questions q on q.id = sa.question_id join survey_phrases sp on sp.survey_answer_id = sa.id join phrases p on p.id = sp.phrase_id where p.id = ? and q.question_number = 3 and sa.answer not like '-'", "#{params[:id]}"])
+  end
+
+  def search_params
+    @search_params ||= begin
+      { page: 1 }.merge(
+        params.permit(
+          :page,
+        ).to_h.symbolize_keys
+      )
+    end
   end
 end
--- app/helpers/phrase_helper.rb
+++ app/helpers/phrase_helper.rb
@@ -7,4 +7,8 @@ module PhraseHelper
       ]
     end
   end
+
+  def highlighted_survey_answer_html(survey_answer, phrase)
+    survey_answer.gsub(/(#{phrase})/i){ |match| "<span class='phrase-highlight'>#{match}</span>" }.html_safe
+  end
 end
