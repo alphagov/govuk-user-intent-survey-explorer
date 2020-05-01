b8d310469acb7f3255f68b32d194eafd1fc1dbf9
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-05-01 11:27:22 +0100

    Extract survey answers for phrase into model

--- spec/features/phrase_usages_spec.rb
+++ spec/features/phrase_usages_spec.rb
@@ -3,7 +3,8 @@ require "spec_helper"
 RSpec.feature "phrase usages" do
   scenario "displays phrase usages which contain the selected phrase" do
     phrase = FactoryBot.create(:phrase, phrase_text: "how government works")
-    survey_answers = FactoryBot.create_list(:survey_answer, 20)
+    survey = FactoryBot.create(:survey, started_at: "2020-04-01")
+    survey_answers = FactoryBot.create_list(:survey_answer, 20, survey: survey)
     survey_answers.each { |survey_answer| FactoryBot.create(:mention, phrase: phrase, survey_answer: survey_answer) }
 
     visit usage_phrase_path(phrase)
