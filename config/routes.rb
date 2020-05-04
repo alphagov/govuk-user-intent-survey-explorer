03ad5eefb3c8a47592441229754546645eacf8a6
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-05-04 15:48:18 +0100

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
   resources :visits, only: %i[index show]
 
   get "summary" => "summary#index", as: :summary
-  # get 'phrase/:id', to: "phrase#show", as: :phrase
   resources :phrases do
     member do
       get "usage"
     end
   end
 
-  # get 'phrase/mentions/:id', to: "number_of_mentions#show", as: :mentions
   resources :mentions, only: [:show]
 
   get "pages/search" => "page_searches#show", as: :page_search
@@ -19,6 +17,5 @@ Rails.application.routes.draw do
   resources :surveys, only: [:show]
 
   resources :pages_visited, only: [:show]
-  # resources :phrase_usage, only: [ :show ]
   mount GovukPublishingComponents::Engine, at: "/component-guide"
 end
